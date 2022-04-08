9cb72b07fe35286a81ad84e2e1fd181b5d1dd35b
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-08 18:14:47 -0300

    181408042022

---
+++ EstruturasSequencial/src/Main.java
@@ -0,0 +1,84 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Expressoes Aritimeticas em Java;
+		 * 
+		 * As expressoes aritimeticas em Java não se diferem em nad do C#, sendo quase que
+		 * as mesmas;
+		 * 
+		 * Exemplos de expressões aritméticas
+				2 * 6 / 3 Resultado = 4
+				3 + 2 * 4 Resultado = 11
+				(3 + 2) * 4 Resultado = 20
+				60 / (3 + 2) * 4 Resultado = 48
+				60 / ((3 + 2) * 4) Resultado = 3
+				
+			Exemplos com o operador "mod"
+				14 % 3 Resultado = 2
+				19 % 5 Resultado = 4
+				############################################################################
+				
+			Variáveis e tipos primitivos em Java	
+			
+			Visão geral
+				• Um programa de computador em execução lida com dados
+				• Como esses dados são armazenados?
+				• Em VARIÁVEIS!
+				
+				Variáveis
+				Definição informal:
+				Em programação, uma variável é uma porção de memória (RAM) utilizada para armazenar 
+				dados durante a execução dos programas.
+				
+			Declaração de variáveis
+			
+				int idade = 25;
+				<tipo> <nome> = <valor inicial>;
+				(opcional)
+				
+				Exemplos:
+				double altura = 1.68;
+				char sexo = 'F';
+				
+			    Uma variável possui:
+				• Nome (ou identificador)
+				• Tipo
+				• Valor
+				• Endereço
+				
+				Tipos primitivos em Java
+				
+				Descrição Tipo Tamanho Valores Valor padrão
+				
+				tipos numéricos inteiros
+				byte 8 bits -128 a 127 0
+				short 16 bits -32768 a 32767 0
+				int 32 bits -2147483648 a 2147483647 0
+				long 64 bits -9223372036854770000 a 
+				9223372036854770000 0L
+				
+				tipos numéricos com ponto flutuante
+				float 32 bits -1,4024E-37 a 3,4028E+38 0.0f
+				double 64 bits -4,94E-307 a 1,79E+308 0.0
+				
+				um caractere Unicode 
+				char 16 bits '\u0000' a '\uFFFF' '\u0000'
+				valor verdade boolean 1 bit {false, true} false
+				
+				Resumo da aula
+				• Conceito informal
+				• Declaração de variáveis: <tipo> <nome> = valor;
+				• Tipos primitivos:
+				• Números inteiros: byte, short, int, long
+				• Números com ponto flutuante: float, double
+				• Valor verdade: boolean
+				• Um caractere Unicode: char
+				• Tipo String: cadeia de caracteres (palavras, textos)
+				• Nomes de variáveis / padrão camel case
+		 * */
+
+	}
+
+}
